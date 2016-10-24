93dbd52b56191e61cbab52a6ceaed273a345d8c5
Roberto Lucha <rlucha@gmail.com>
2016-10-24 19:23:06 +0200

    Adds travis, form links and form tags.

---
+++ deploy.js
@@ -0,0 +1,40 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+var args = process.argv.slice(2);
+
+
+var config = {
+    username: process.env.USER,
+    host: "ftp.oxytours.com",
+    password: process.env.PASS,
+    port: 21,
+    localRoot: __dirname + "/static",
+    remoteRoot: "/web/",
+    exclude: ['.git', '.idea', 'tmp/*']
+}
+
+var config_mobile = {
+    username: process.env.USER,
+    host: "ftp.oxytours.com",
+    password: process.env.PASS,
+    port: 21,
+    localRoot: __dirname + "/mobile",
+    remoteRoot: "/web/mobile",
+    exclude: ['.git', '.idea', 'tmp/*']
+}
+
+if(args[0] === 'desktop') {
+  ftpDeploy.deploy(config, function(err) {
+      if (err) console.log(err)
+      else console.log('finished');
+  });
+} else if (args[0] === 'mobile') {
+  ftpDeploy.deploy(config_mobile, function(err) {
+      if (err) console.log(err)
+      else console.log('finished');
+  });
+}
+
+ftpDeploy.on('uploading', function(data) {
+    process.stdout.write(`Uploading ${args[0]}: ${data.percentComplete}% complete\r`);
+});
