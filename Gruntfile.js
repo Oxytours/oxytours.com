a5cb55689c1e52e15b75009249ece77557c1a6b9
Roberto Lucha <rlucha@gmail.com>
2016-12-23 02:39:38 +0100

    Added ftpsync

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         sass: {
             options: {
-                sourceMap: true
-            },
+                sourceMap: true,
+              },
             dist: {
               files: {
                   'static/css/app.css': 'scss/app.scss',
-                  'mobile/css/app_mobile.css': 'scss/app_mobile.scss'
-              },
-            }
-        },
+                  'mobile/css/app_mobile.css': 'scss/app_mobile.scss',
+                },
+            },
+          },
 
         assemble: {
             options: {
@@ -21,52 +21,52 @@ module.exports = function(grunt) {
                 helpers: ['node_modules/handlebars-helpers/index.js'],
                 partials: ['views/partials/**/*.hbs'],
                 data: ['data/**/*.json'],
-                flatten: true
-            },
+                flatten: true,
+              },
             main_es: {
                 src:   ['views/pages/es/**/*.hbs'],
-                dest:  'static/es/'
-            },
+                dest:  'static/es/',
+              },
             main_en: {
                 src:   ['views/pages/en/**/*.hbs'],
-                dest:  'static/en/'
-            },
+                dest:  'static/en/',
+              },
             main_fr: {
                 src:   ['views/pages/fr/**/*.hbs'],
-                dest:  'static/fr/'
-            },
+                dest:  'static/fr/',
+              },
             mobile_es: {
                 options: {
-                    layout: 'layouts/default_mobile.hbs'
-                },
+                    layout: 'layouts/default_mobile.hbs',
+                  },
                 src:   ['views/pages/es/**/*.hbs'],
-                dest:  'mobile/es/'
-            },
+                dest:  'mobile/es/',
+              },
             mobile_en: {
                 options: {
-                    layout: 'layouts/default_mobile.hbs'
-                },
+                    layout: 'layouts/default_mobile.hbs',
+                  },
                 src:   ['views/pages/en/**/*.hbs'],
-                dest:  'mobile/en/'
-            },
+                dest:  'mobile/en/',
+              },
             mobile_fr: {
                 options: {
-                    layout: 'layouts/default_mobile.hbs'
-                },
+                    layout: 'layouts/default_mobile.hbs',
+                  },
                 src:   ['views/pages/fr/**/*.hbs'],
-                dest:  'mobile/fr/'
-            }
-        },
+                dest:  'mobile/fr/',
+              },
+          },
 
         connect: {
             server: {
                 options: {
                     port: 8081,
                     base: './',
-                    hostname:'*',
-                }
-            }
-        },
+                    hostname: '*',
+                  },
+              },
+          },
 
         watch: {
             sass: {
@@ -74,25 +74,25 @@ module.exports = function(grunt) {
                 tasks: ['sass'],
                 options: {
                     livereload: 1337,
-                }
-            },
+                  },
+              },
 
             assemble: {
-                files: ['views/**/*','scss/**/*.scss'],
+                files: ['views/**/*', 'scss/**/*.scss'],
                 tasks: ['assemble'],
                 options: {
                     livereload: 1337,
-                }
-            }
-        },
+                  },
+              },
+          },
 
         clean: {
-            all: ['static/es/**/*.html','static/en/**/*.html','static/fr/**/*.html']
-        },
+            all: ['static/es/**/*.html', 'static/en/**/*.html', 'static/fr/**/*.html'],
+          },
 
         run: {
           commands: {
-            exec: 'node deploy desktop && node deploy mobile',
+            exec: 'ftpsync -h ftp.oxytours.com -p 21 -u $OX_USER -s $OX_PASS -l static -r web/static && ftpsync -h ftp.oxytours.com -p 21 -u $OX_USER -s $OX_PASS -l mobile -r web/mobile',
           }
         }
 
